d414ed74119f0d0299ce3c337f3a9c8121d09542
cdonnay <christopherdonnay@gmail.com>
2016-11-14 22:38:34 -0800

    Update Imputation.R
    I added the forest lines for a prediction using a default forest. Not sure how it'll work with this data.

--- Imputation.R
+++ Imputation.R
@@ -1,7 +1,10 @@
+library(randomForest)
 Train <- read.csv('cs-training.csv', header=TRUE,comment.char="")
 Train$X <- NULL # Removes redundant indexing
 N_var <- length(colnames(Train)) # Number of variables
 N_obs <- length(Train$age) # Number of Observations
+Test <- read.csv('cs-test.cv', header=TRUE, comment.char="")
+Test$X <- NULL # Removes redundant indexing
 
 Update_MI <- function(df) {
   # Updates 0 and NA values of Monthly Income
@@ -39,11 +42,12 @@ Update_DR <- function(df) {
   #
   # Returns:
   #   dataframe w/ updated values
-  LinReg <- lm(DebtRatio ~ .,df) # Performs linear regression for Monthly Income using all other variales
+  LinReg <- lm(DebtRatio ~ .,df) # Performs linear regression for Debt Ratio using all other variales
   df$DebtRatio[zero.na.MI.index] <- predict(LinReg,df[zero.na.MI.index,]) # Imputes for Debt Ratio
   #print(paste("DR: ",df$DebtRatio[zero.na.MI.index[1]]),sep="") # Check for convergence
   return(df)
 }
+
 
 Impute <- function(df) {
   # Imputes NA values in the data frame using linear regression
@@ -84,4 +88,7 @@ Impute <- function(df) {
 
 Train <- Impute(Train)
 
+train.Forest<-randomForest(SeriousDlqin2yrs ~ ., Train)
+
+predict(train.Forest, Test)
 
